efccfd60bb49fcf29b449245f41b8a030c124eb3
mor <morning_717@163.com>
2019-09-03 16:56:29 +0800

    增加行分数

--- src/component/subface.tsx
+++ src/component/subface.tsx
@@ -450,7 +450,7 @@ class Subface extends React.Component<any,any> {
     }
 
     randomRow(){
-        // 逻辑待处理
+        // 逻辑待处理 增加bool控制是否在1000分段内增加过 当前分数%1000
         this.state.subfaceModel.subface.splice(0,1);
 
         this.state.subfaceModel.subface.unshift();
