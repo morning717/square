cdb80da5a722c5fc94e876757a4558a5f43262ce
mor <morning_717@163.com>
2019-09-05 17:02:35 +0800

    todo

--- src/component/subface.tsx
+++ src/component/subface.tsx
@@ -442,7 +442,7 @@ class Subface extends React.Component<any,any> {
     getPoint(){
         // 处理分数达到一定值 底部随机添加一行
         if (this.state.point.curPoint >= 100){
-            this.randomRow();
+            // this.randomRow();
         }
         // 下落速度待优化处理 (新方案)
         this.state.timeInterval.interval = (this.state.timeInterval.interval * (1 - this.state.point.curPoint / 1000 /100)) < 300 ? 300:(this.state.timeInterval.interval * (1 - this.state.point.curPoint / 1000 /100));
